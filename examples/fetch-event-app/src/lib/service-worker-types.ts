cb6911a3ccfeecc81b3536024e194d7d87f65860
rajsite <1588923+rajsite@users.noreply.github.com>
2025-04-30 16:14:14 -0500

    Tsconfig leverage module format types

--- examples/fetch-event-app/src/lib/service-worker-types.ts
+++
@@ -1,5 +0,0 @@
-// To support service worker types
-// See: https://github.com/microsoft/TypeScript/issues/14877
-/// <reference lib="webworker" />
-export const addEventListener = (globalThis as unknown as ServiceWorkerGlobalScope).addEventListener.bind(globalThis);
-export type FetchListener = (this: ServiceWorkerGlobalScope, ev: ServiceWorkerGlobalScopeEventMap['fetch']) => unknown;
--- examples/fetch-event-app/src/main.ts
+++ examples/fetch-event-app/src/main.ts
@@ -1,6 +1,18 @@
 import { handle } from './lib/handle';
-import { addEventListener } from './lib/service-worker-types';
 
-addEventListener('fetch', (event: FetchEvent): void => event.respondWith(
-  handle(event.request)
-));
+// Manually define FetchEvent types based on:
+// https://github.com/microsoft/TypeScript/blob/v5.8.3/src/lib/webworker.generated.d.ts#L2956
+// See: https://github.com/microsoft/TypeScript/issues/14877#issuecomment-2843268722
+declare global {
+  interface WindowEventMap {
+    fetch: FetchEvent;
+  }
+}
+
+interface FetchEvent extends Event {
+  readonly request: Request;
+  readonly resultingClientId: string;
+  respondWith(r: Response | PromiseLike<Response>): void;
+}
+
+addEventListener('fetch', e => e.respondWith(handle(e.request)));
