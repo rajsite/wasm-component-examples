91049cfc0cbf9d51cb770188c7bdf65a81dc98d8
rajsite <rajsite@users.noreply.github.com>
2026-03-26 00:08:47 -0500

    Switch to fully qualified export name

--- examples/fs-export/src/main.ts
+++ examples/fs-export/src/main.ts
@@ -2,7 +2,10 @@ import type { run as Run } from 'wasi:cli/run@0.2.3';
 import { run as runIn } from 'wasi:cli/run@0.2.3';
 // import { handle } from './handle';
 
-export { preopens, types } from './fs';
+export {
+    preopens as 'wasi:filesystem/preopens@0.2.3',
+    types as 'wasi:filesystem/types@0.2.3'
+} from './fs';
 
 export const run: {
     run: typeof Run
